faa10df8ef619115517f1e669de45f0f08fab444
Ismar Diaz <70118333+izimar@users.noreply.github.com>
2022-08-03 10:44:51 -0400

    Update Ip-Matcher.py

--- Ip-Matcher.py
+++ Ip-Matcher.py
@@ -10,21 +10,25 @@ with open('Pcap-Ips.csv') as csvfile:
         Pcap_Ips.append(row[2])
 
 
-with open('VT-Contacted-IPs.csv') as csvfile:
+with open('VT-test.csv') as csvfile:
     Ip_Find = csv.reader(csvfile)
     for row in Ip_Find:
         for r in row:
-            IP_Length = len(r)
+            row_Length = len(r)
             IP= ""
             num = 0
-            while num < IP_Length:
+            while num < row_Length:
                 for char in r:
                     num+=1
                     if char in ["0","1","2","3","4","5","6","7","8","9","."]:
                         IP += char
-                    if char == ":" and num > 6:
+                        print(len(IP))
+                    if char in [":","/"] and len(IP) >= 7:
+                        num = row_Length
                         break
+                
             Mal_Ips.append(IP)
+            
 
 def IP_Check(Pcap_Ips,Mal_Ips,Ip_Matches):
     for I in Pcap_Ips:
@@ -40,3 +44,4 @@ def IP_Check(Pcap_Ips,Mal_Ips,Ip_Matches):
         print("There were no matching IPs")
         
 IP_Check(Pcap_Ips,Mal_Ips,Ip_Matches)
+print(Mal_Ips)
